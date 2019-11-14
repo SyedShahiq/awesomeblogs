21ae18802a34922ef83a5b319c2f5a91b0885496
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-14 14:29:06 +0500

    Posts views added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home_view(request,*arg,**kwargs):
 	return render(request,'home.html',{})
+
+def posts_view(request,*arg,**kwargs):
+	posts = Post.objects.all()
+	return render(request,'posts.html',{'posts':posts})
