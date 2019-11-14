49a8974a61549071aa5e7a3fec6dbdb005e3111a
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-14 14:29:46 +0500

    urls added

--- blog_admin/urls.py
+++ blog_admin/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import home_view
+from blog.views import home_view,posts_view
 
 urlpatterns = [
 	path('',home_view,name='home'),
+	path('posts/',posts_view,name='posts'),
   path('admin/', admin.site.urls),
 ]
