edfa0ddc6584f5d6b46f4d4b03f32b9ef0b24d82
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-15 14:08:04 +0500

    Display comments in posts details

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Post
+from comments.models import Comment
 
 # Create your views here.
 def home_view(request,*arg,**kwargs):
@@ -12,10 +13,13 @@ def posts_view(request,*arg,**kwargs):
 def single_post_view(request,id):
 	try:
 		post = Post.objects.get(id=id)
+		comments = Comment.objects.filter(post=id)
+		print(comments)
 	except Post.DoesNotExist:
 		return redirect("/")
 
 	context = {
-		'post':post
+		'post':post,
+		'comments':comments
 	}
 	return render(request,'posts_detail.html',context)
