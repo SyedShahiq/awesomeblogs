5a0dff0d106a5e780395f53ef0a3575188a02c6f
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-18 12:46:57 +0500

    user model added for comments

--- comments/models.py
+++ comments/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from blog.models import Post
+from django.contrib.auth.models import User
 # Create your models here.
 class Comment(models.Model):
 	content = models.TextField()
 	date_posted = models.DateTimeField(default = timezone.now)
 	post = models.ForeignKey(Post, on_delete = models.CASCADE)
+	user = models.ForeignKey(User,on_delete = models.CASCADE,default=1)
