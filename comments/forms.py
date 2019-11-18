d87579378d8a211c707622599df75597ebea7934
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-18 13:48:29 +0500

    Updated the content title for comments

--- comments/forms.py
+++ comments/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Comment
 #Django Model Form
 class commentForm(forms.ModelForm):
+	content = forms.CharField(label='Comment',widget=forms.Textarea({
+		'placeholder':'Comment',
+		'class': 'form-group'
+		}))
 	class Meta:
 		model = Comment
 		fields = [
