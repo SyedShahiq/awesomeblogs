3846c10c60e9cb8180226ae53bf47860557b2f09
Syed Shahiq <shahiqurrehman@gmail.com>
2019-11-19 11:32:25 +0500

    Error handled for replies

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
+	$('.comments .error').hide();
 	$('.reply-btn').click(function(){
 		var get_comment_id = $(this).parent('.col-sm-2').parent('.row').find('.reply').attr('id');
 		var get_reply_content = $(this).parent('.col-sm-2').parent('.row').find('.reply').val();
-		$.ajax({
-			contentType: 'application/json',
-			data:  JSON.stringify({ "comment_id": get_comment_id , "content":get_reply_content }),
-			dataType: 'json',
-			type: 'POST',
-			url: '/create-new-reply/',
-			headers: { "X-CSRFToken": $.cookie("csrftoken") },
-			success: function(data){
-				window.location.reload();
-			},
-			error: function(){
-				console.log("Device control failed");
-			},
-		});
+		if (get_reply_content == '') {
+			$(this).parent('.col-sm-2').parent('.row').find('.reply').addClass('is-invalid');
+			$(this).parent('.col-sm-2').parent('.row').find('.error').show();
+		} else {
+			$.ajax({
+				contentType: 'application/json',
+				data:  JSON.stringify({ "comment_id": get_comment_id , "content":get_reply_content }),
+				dataType: 'json',
+				type: 'POST',
+				url: '/create-new-reply/',
+				headers: { "X-CSRFToken": $.cookie("csrftoken") },
+				success: function(data){
+					window.location.reload();
+				},
+				error: function(){
+					console.log("Device control failed");
+				},
+			});
+		}
 	})
 });
